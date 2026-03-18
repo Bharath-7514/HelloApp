396d3ff951cccbc41e7fb3ae041a783882cfcaa4
Bharath-7514 <bc7234@srmist.edu.in>
2026-03-18 10:11:12 +0530

    feature/UC7

--- HelloApp.java
+++ HelloApp.java
@@ -4,13 +4,9 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "";
-            for (String name : args) {
-                result += name + ", ";
-            }
-            result = result.substring(0, result.length() - 2);
+            String names = String.join(", ", args);
 
-            System.out.println("Hello, " + result + "!");
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
