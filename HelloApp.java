f1a272b42d2d76d2826b2b3f29be374e8b3fd03c
Bharath-7514 <bc7234@srmist.edu.in>
2026-03-18 09:29:06 +0530

    UC3

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+	String name = "World"; 
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello");
-        }
-
+            name = args[0];
+        } 
+		
+		System.out.println("Hello, " + name + "!");
     }
 }
